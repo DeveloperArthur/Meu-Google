a6ceb9c42b1e98eb4274c10513310e9d9c7de070
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-10-15 20:59:52 -0300

    Update pesquisar.js

--- js/pesquisar.js
+++ js/pesquisar.js
@@ -1,7 +1,5 @@
 function pesquisar(){
 	var pesquisa = document.getElementById("pesquisa").value;
-	if(pesquisa.trim().isEmpty()){
-		urlDePesquisa = "https://www.google.com.br/search?source=hp&ei=OBamXZusAo6v5OUPjdCfmAc&q="+pesquisa;
-		window.location.href = urlDePesquisa;
-	}
+        urlDePesquisa = "https://www.google.com.br/search?source=hp&ei=OBamXZusAo6v5OUPjdCfmAc&q="+pesquisa;
+	window.location.href = urlDePesquisa;
 }
